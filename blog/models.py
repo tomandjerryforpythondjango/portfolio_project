b291b4d97770b2692d11b584f148ec9639ba21f0
tomandjerryforpythondjango <tomandjerryforpythondjango@gmail.com>
2021-02-13 17:21:09 +0530

    Complete Portfolio website

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,12 @@ class Blog(models.Model):
 	publish_date = models.DateTimeField()
 	body = models.TextField()
 	image = models.ImageField(upload_to='images/')
+
+	def __str__(self):
+		return self.title
+		
+	def summary(self):
+		return self.body[:200] + '...'
+
+	def pub_date(self):
+		return self.publish_date.strftime('%b %e %Y')
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from blog.models import Blog
 
-# Create your views here.
+def allblogs(request):
+	blogs = Blog.objects.all()
+	return render(request, 'blog/allblogs.html', {'blogs': blogs})
+
+def detail(request, blog_id):
+	detail_blog = get_object_or_404(Blog, pk=blog_id)
+	return render(request, 'blog/detail.html', {'detail_blog': detail_blog})
