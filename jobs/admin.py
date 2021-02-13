abbd04309a9e1deee263501c7686ec5b9d259958
tomandjerryforpythondjango <tomandjerryforpythondjango@gmail.com>
2021-02-13 13:12:08 +0530

    created jobs and reflecting on home page

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from jobs.models import Job
 # Register your models here.
+class JobAdmin(admin.ModelAdmin):
+	class Meta:
+		model = Job
+		fields = '__all__'
+
+admin.site.register(Job, JobAdmin)
--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from jobs.models import Job
 
-# Create your views here.
+
+def home(request):
+	jobs = Job.objects.all()
+	return render(request, 'jobs/home.html', {'jobs':jobs})
